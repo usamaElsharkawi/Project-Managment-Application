d776ba0e15d928662869f5804f07a84026f7f9ea
Usama El-Sharkawi <usamasharkawi@gmail.com>
2025-12-15 16:32:11 +0200

    Styling Buttons &inputs with tailwind CSS

--- src/assets/components/Input.jsx
+++ src/assets/components/Input.jsx
@@ -1,8 +1,9 @@
 export default function Input({ label, textArea, ...props }) {
+    const calsses = 'w-full p-1 rounded-sm border-b-2 border-stone-300 bg-stone-200 focus:outline-none focus:border-stone-600'
   return (
-    <p>
-      <label>{label}</label>
-      {textArea ? <textarea {...props} /> : <input {...props} />}
+    <p className=" flex flex-col gap-1 my-4 " >
+      <label className="uppercase text-sm font-bold text-stone-500">{label}</label>
+      {textArea ? <textarea className={calsses} {...props} /> : <input className={calsses} {...props} />}
     </p>
   );
 }
--- src/assets/components/NewProject.jsx
+++ src/assets/components/NewProject.jsx
@@ -1,9 +1,9 @@
 import Input from "./Input"
 export default function NewPorject(){
     return <div>
-        <menu>
-            <button>Cancel</button>
-            <button>Save</button>
+        <menu className=" w-[35rem] mt-16 flex justify-end items-center gap-4 my-4 " >
+            <button className=" text-stone-800 hover:text-stone-950 " >Cancel</button>
+            <button className="bg-slate-800 text-stone-50 py-2 px-6 rounded-md hover:bg-stone-950" >Save</button>
         </menu>
         <div>
             <Input label='title'/>
